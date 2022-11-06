fbc7e16a7847eeb2baae79da90793652582a7fa4
Ramiro Pruis <82285826+RamiroPruis@users.noreply.github.com>
2022-11-06 20:52:43 -0300

    static files

---
+++ gateway.js
@@ -0,0 +1,3 @@
+export default function apiHandler(req,res){
+    
+}
---
+++ static/main.js
@@ -0,0 +1,4 @@
+
+document.getElementById("sucursal").addEventListener("click",()=>{
+    document.querySelector(".container").innerHTML += "<h2>PAPAPAPAP</h2>"
+})
