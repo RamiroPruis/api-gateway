66841f6671eec968fc5ccc931161bef9be7bd2ae
JoaquinFioriti <joaquinfioritifreelance@gmail.com>
2022-11-08 20:17:31 -0300

    Done el post

--- apiHandler.js
+++ apiHandler.js
@@ -6,8 +6,8 @@ const RESERVAS_PORT = 2001
 
 
 const RESERVAS_HANDLER = {
-    GET: get,
-    // POST: post,
+    GET: getReservas,
+    POST: postReservas
     // DELETE: del
 }
 
@@ -20,18 +20,18 @@ export const apiHandler = async(req, res) =>{
     const url = req.url.split('/')[2]
     const {method} = req
 
-    if (url == "reservas"){
+    if (req.url.includes("reservas")){
         console.log("Entramos en reservas")
         RESERVAS_HANDLER[method](req,res)
     }
-    else if (url == "sucursales"){
+    else if (req.url.includes("sucursales")){
         console.log("Entramos en sucursales")
         SUCURSALES_HANDLER[method](req,res)
     }
     else errorHandler(400,"Endpoint no valido",res)
 }
 
-function get (req,res){
+function getReservas(req,res){
 
     
     const startsApi = req.url.indexOf('/api')
@@ -46,7 +46,7 @@ function get (req,res){
             'Content-type': 'application/json'
         }
     }
-    console.log("Options: ", options)
+    
 
     const reqReservas = http.request(options,(resReservas)=>{
         let data = []
@@ -57,7 +57,6 @@ function get (req,res){
 
         resReservas.on('end',()=>{
             let body = JSON.parse(Buffer.concat(data).toString())
-            console.log("Hicimos el get: obtuvimos este body:" , body )
             res.writeHead(200,{'Content-Type': 'application/json'})
             res.end(JSON.stringify(body))
         })
@@ -66,6 +65,54 @@ function get (req,res){
     reqReservas.write(JSON.stringify({})) 
     reqReservas.end()
 
+}
+
+
+function postReservas (req,res){
+
+    let body = []
+
+    req.on('data', (chunck)=>body.push(chunck))
+
+    req.on('end',()=>{
+
+        body = JSON.parse(Buffer.concat(body).toString())
+        const startsApi = req.url.indexOf('/api')
+        const path = req.url.substring(startsApi, req.url.length)
+
+        const options = {
+            host:'localhost',
+            port: RESERVAS_PORT,
+            path: path,       
+            method: 'POST',
+            headers:{
+                'Content-type': 'application/json'
+            }
+        }
+
+        const reservaReq = http.request(options, (resReserva)=>{
+
+            let reservaBody = []
+            
+            resReserva.on('data', (chunck)=>reservaBody.push(chunck))
+
+            resReserva.on('end',()=>{
+                reservaBody = JSON.parse(Buffer.concat(reservaBody).toString())
+                res.writeHead(200,{'Content-Type': 'application/json'})
+                res.end(JSON.stringify(reservaBody))
+            })
+        })
+
+        reservaReq.write(JSON.stringify(body)) 
+        reservaReq.end()
+
+
+
+
+    })
     
+    
+
+
 
 }
