a63a77ddffbbcd08a25cf34f5761512bc95c6c76
Ramiro Pruis <82285826+RamiroPruis@users.noreply.github.com>
2022-11-10 17:39:26 -0300

    start front

--- static/main.js
+++ static/main.js
@@ -1,4 +1,41 @@
+const form = document.getElementById("reservas-form")
+const reservaButton = document.getElementById("reserva-button")
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+const reservaConfirm = document.getElementById("reserva-confirm")
 
-document.getElementById("sucursal").addEventListener("click",()=>{
-    document.querySelector(".container").innerHTML += "<h2>PAPAPAPAP</h2>"
+
+
+form.addEventListener("submit",(ev)=>{
+    ev.preventDefault()
+    
 })
+
+
+reservaButton.onclick = ()=>{
+    modal.style.display = "block"
+}
+
+span.onclick = function() {
+    modal.style.display = "none"
+  }
+  
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none"
+    }
+  }
+
+reservaConfirm.onclick = ()=>{
+    const obj = {}
+    const formData = new FormData(form)
+    for (const key of formData.keys()) {
+        obj[key] = formData.get(key)
+      }
+
+      obj.userId = 0
+
+      console.log(obj)
+
+    modal.style.display = "none"
+}
