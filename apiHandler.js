cdc79fdf4c2d4fd504a8841a1d79b05623167b42
Ramiro Pruis <82285826+RamiroPruis@users.noreply.github.com>
2022-11-14 16:21:46 -0300

    avances paqui

--- apiHandler.js
+++ apiHandler.js
@@ -1,151 +1,128 @@
-import http from 'http'
-import {errorHandler} from './error.js'
+import http from "http";
+import { errorHandler } from "./error.js";
 
-const SUCURSALES_PORT = 2000
-const RESERVAS_PORT = 2001
-
+const SUCURSALES_PORT = 2000;
+const RESERVAS_PORT = 2001;
 
 const RESERVAS_HANDLER = {
-    GET: getReservas,
-    POST: postReservas
-    // DELETE: del
-}
+  GET: getReservas,
+  POST: postReservas,
+  // DELETE: del
+};
 
 const SUCURSALES_HANDLER = {
-    GET: getSucursales,
+  GET: getSucursales,
+};
+
+export const apiHandler = async (req, res) => {
+  const url = req.url.split("/")[2];
+  const { method } = req;
+
+  if (req.url.includes("reservas")) {
+    console.log("Entramos en reservas");
+    RESERVAS_HANDLER[method](req, res);
+  } else if (req.url.includes("sucursales")) {
+    console.log("Entramos en sucursales");
+    SUCURSALES_HANDLER[method](req, res);
+  } else errorHandler(400, "Endpoint no valido", res);
+};
+
+function getSucursales(req, res) {
+  const startsApi = req.url.indexOf("/api");
+  const path = req.url.substring(startsApi, req.url.length);
+
+  const options = {
+    host: "localhost",
+    port: SUCURSALES_PORT,
+    path: path,
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+
+  const reqSucursales = http.request(options, (resSucursales) => {
+    let data = [];
+
+    resSucursales.on("data", (chunck) => data.push(chunck));
+
+    resSucursales.on("end", () => {
+      let body = JSON.parse(Buffer.concat(data).toString());
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(body));
+    });
+  });
+
+  reqSucursales.write(JSON.stringify({}));
+  reqSucursales.end();
 }
 
-export const apiHandler = async(req, res) =>{
+function getReservas(req, res) {
+  const startsApi = req.url.indexOf("/api");
+  const path = req.url.substring(startsApi, req.url.length);
 
-    const url = req.url.split('/')[2]
-    const {method} = req
+  const options = {
+    host: "localhost",
+    port: RESERVAS_PORT,
+    path: path,
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
 
-    if (req.url.includes("reservas")){
-        console.log("Entramos en reservas")
-        RESERVAS_HANDLER[method](req,res)
-    }
-    else if (req.url.includes("sucursales")){
-        console.log("Entramos en sucursales")
-        SUCURSALES_HANDLER[method](req,res)
-    }
-    else errorHandler(400,"Endpoint no valido",res)
+  console.log(options);
+  const reqReservas = http.request(options, (resReservas) => {
+    let data = [];
+
+    resReservas.on("data", (chunk) => {
+      data.push(chunk);
+    });
+
+    resReservas.on("end", () => {
+      let body = JSON.parse(Buffer.concat(data).toString());
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(body));
+    });
+  });
+
+  reqReservas.write(JSON.stringify({}));
+  reqReservas.end();
 }
 
-function getSucursales(req,res){
+function postReservas(req, res) {
+  let body = [];
 
-    const startsApi = req.url.indexOf('/api')
-    const path = req.url.substring(startsApi, req.url.length)
+  req.on("data", (chunck) => body.push(chunck));
+
+  req.on("end", () => {
+    body = JSON.parse(Buffer.concat(body).toString());
+    const startsApi = req.url.indexOf("/api");
+    const path = req.url.substring(startsApi, req.url.length);
 
     const options = {
-        host:'localhost',
-        port: SUCURSALES_PORT,
-        path: path,       
-        method: 'GET',
-        headers:{
-            'Content-type': 'application/json'
-        }
-    }
+      host: "localhost",
+      port: RESERVAS_PORT,
+      path: path,
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-    const reqSucursales = http.request(options, (resSucursales)=>{
-        let data = []
+    const reservaReq = http.request(options, (resReserva) => {
+      let reservaBody = [];
 
-        resSucursales.on('data',(chunck) => data.push(chunck))
+      resReserva.on("data", (chunck) => reservaBody.push(chunck));
 
-        resSucursales.on('end',()=>{
-            let body = JSON.parse(Buffer.concat(data).toString())
-            res.writeHead(200,{'Content-Type': 'application/json'})
-            res.end(JSON.stringify(body))
-        })
-    })
+      resReserva.on("end", () => {
+        reservaBody = JSON.parse(Buffer.concat(reservaBody).toString());
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(reservaBody));
+      });
+    });
 
-       
-    reqSucursales.write(JSON.stringify({})) 
-    reqSucursales.end()
-    
-}
-
-function getReservas(req,res){
-
-    
-    const startsApi = req.url.indexOf('/api')
-    const path = req.url.substring(startsApi, req.url.length)
-    
-    const options = {
-        host:'localhost',
-        port: RESERVAS_PORT,
-        path: path,       
-        method: 'GET',
-        headers:{
-            'Content-type': 'application/json'
-        }
-    }
-    
-
-    const reqReservas = http.request(options,(resReservas)=>{
-        let data = []
-    
-        resReservas.on('data',(chunk)=>{
-            data.push(chunk)
-        })
-
-        resReservas.on('end',()=>{
-            let body = JSON.parse(Buffer.concat(data).toString())
-            res.writeHead(200,{'Content-Type': 'application/json'})
-            res.end(JSON.stringify(body))
-        })
-    })
-   
-    reqReservas.write(JSON.stringify({})) 
-    reqReservas.end()
-
-}
-
-
-function postReservas (req,res){
-
-    let body = []
-
-    req.on('data', (chunck)=>body.push(chunck))
-
-    req.on('end',()=>{
-
-        body = JSON.parse(Buffer.concat(body).toString())
-        const startsApi = req.url.indexOf('/api')
-        const path = req.url.substring(startsApi, req.url.length)
-
-        const options = {
-            host:'localhost',
-            port: RESERVAS_PORT,
-            path: path,       
-            method: 'POST',
-            headers:{
-                'Content-type': 'application/json'
-            }
-        }
-
-        const reservaReq = http.request(options, (resReserva)=>{
-
-            let reservaBody = []
-            
-            resReserva.on('data', (chunck)=>reservaBody.push(chunck))
-
-            resReserva.on('end',()=>{
-                reservaBody = JSON.parse(Buffer.concat(reservaBody).toString())
-                res.writeHead(200,{'Content-Type': 'application/json'})
-                res.end(JSON.stringify(reservaBody))
-            })
-        })
-
-        reservaReq.write(JSON.stringify(body)) 
-        reservaReq.end()
-
-
-
-
-    })
-    
-    
-
-
-
+    reservaReq.write(JSON.stringify(body));
+    reservaReq.end();
+  });
 }
--- static/main.js
+++ static/main.js
@@ -1,41 +1,87 @@
 const form = document.getElementById("reservas-form")
 const reservaButton = document.getElementById("reserva-button")
-const modal = document.getElementById("myModal");
-const span = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("myModal")
+const span = document.getElementsByClassName("close")[0]
 const reservaConfirm = document.getElementById("reserva-confirm")
+const sucursalContainer = document.getElementById("reserva-sucursal")
 
+const SUCURSALES_PORT = 2000
+const RESERVAS_PORT = 2001 //2
 
+const getSucursales = async () => {
+  const reqSucursales = await fetch("http://localhost:8000/api/sucursales/")
 
-form.addEventListener("submit",(ev)=>{
-    ev.preventDefault()
-    
-})
-
-
-reservaButton.onclick = ()=>{
-    modal.style.display = "block"
+  return reqSucursales.json()
 }
 
-span.onclick = function() {
-    modal.style.display = "none"
-  }
-  
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none"
+getSucursales().then((res) => {
+  res.forEach((sucursal) => {
+    document.querySelector(
+      "#reserva-sucursal"
+    ).innerHTML += `<option value=${sucursal.id}> ${sucursal.name}</option>`
+  })
+})
+
+const getReservas = async (params) => {
+  console.log("holis")
+  const req = await fetch("http://localhost:8000/api/reservas?" + params)
+  return req.json()
+}
+
+sucursalContainer.addEventListener("change", () => {
+  let sucursal = document.querySelector("#reserva-sucursal").value
+
+  console.log(
+    "http://localhost:8000/api/reservas?" +
+      new URLSearchParams({
+        branchId: sucursal,
+      })
+  )
+
+  getReservas(new URLSearchParams({ branchId: sucursal, userId: -1 })).then(
+    (res) => {
+      console.log(res)
+      document.querySelector("#reserva-horario").innerHTML = ""
+      res.forEach(
+        (reserva) =>
+          (document.querySelector(
+            "#reserva-horario"
+          ).innerHTML += `<option value=${reserva.id}> ${new Date(
+            reserva.dateTime
+          )}</option>`)
+      )
     }
+  )
+})
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault()
+})
+
+reservaButton.onclick = () => {
+  modal.style.display = "block"
+}
+
+span.onclick = function () {
+  modal.style.display = "none"
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+}
+
+reservaConfirm.onclick = () => {
+  const obj = {}
+  const formData = new FormData(form)
+  for (const key of formData.keys()) {
+    obj[key] = formData.get(key)
   }
 
-reservaConfirm.onclick = ()=>{
-    const obj = {}
-    const formData = new FormData(form)
-    for (const key of formData.keys()) {
-        obj[key] = formData.get(key)
-      }
+  obj.userId = 0
 
-      obj.userId = 0
+  console.log(obj)
 
-      console.log(obj)
-
-    modal.style.display = "none"
+  modal.style.display = "none"
 }
